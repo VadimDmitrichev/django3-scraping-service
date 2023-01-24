fe364b3208bcbfc741ca135c1e11dc06e07ea955
Vadim <vadya.dmitrichev26@gmail.com>
2023-01-24 22:36:39 +0300

    Фильтрация поиска по городу и ЯП

--- scraping/views.py
+++ scraping/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home(request):
 	'''Функция для отображения вакансий'''
-	query = Vacancy.objects.all()
-	return render(request, 'scraping/home.html', {'vacancy_list': query})
+
+	form = FindForm()
+	city = request.GET.get('city')
+	language = request.GET.get('language')
+	query = []
+	if city or language:
+		d_filter = {}
+		if city:
+			d_filter['city__slug'] = city
+		if language:
+			d_filter['language__slug'] = language
+		query = Vacancy.objects.filter(**d_filter)
+	return render(request, 'scraping/home.html', {'vacancy_list': query, 'form': form})
