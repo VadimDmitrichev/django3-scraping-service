ab371aaeefa284826433ad41a344a6fc2846f93b
Vadim <vadya.dmitrichev26@gmail.com>
2023-01-30 11:39:54 +0300

    Готовый парсинг через асинхронную функцию

--- run_scraping.py
+++ run_scraping.py
@@ -1,7 +1,9 @@
+import asyncio
 import codecs
 import os
 import sys
 
+from django.contrib.auth import get_user_model
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
@@ -11,21 +13,76 @@ import django
 
 django.setup()
 
-from scraping.models import City
+from django.db import DatabaseError
+from scraping.models import City, Language, Vacancy, Error, Url
 from scraping.parsers import *
 
+User = get_user_model()
 
 parsers = (
-	(hh_ru, 'https://hh.ru/search/vacancy?text=python&area=1'),
-	(habr_career, 'https://career.habr.com/vacancies?q=python&type=all'),
+	(hh_ru, 'hh_ru'),
+	(habr_career, 'habr_career'),
 )
-city = City.objects.filter(slug='kursk')
-jobs, errors = [], []
-for func, url in parsers:
-	j, e = func(url)
-	jobs += j
-	errors += e
 
-h = codecs.open('work.txt', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
+jobs, errors = [], []
+
+
+def get_settings():
+	"""Функция которая возвращает ключи для поиска по яп и городу"""
+	qs = User.objects.filter(send_email=True).values()
+	settings_list = set((q['city_id'], q['language_id']) for q in qs)
+	return settings_list
+
+
+def get_urls(_settings):
+	"""Функция которая возвращает набор url по ключам полученным из get_settings"""
+	qs = Url.objects.all().values()
+	url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+	urls = []
+	for pair in _settings:
+		tmp = {'city': pair[0], 'language': pair[1], 'url_data': url_dict[pair]}
+		urls.append(tmp)
+	return urls
+
+
+async def main(value):
+	func, url, city, language = value
+	job, error = await loop.run_in_executor(None, func, url, city, language)
+	errors.extend(error)
+	jobs.extend(job)
+
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+loop = asyncio.get_event_loop()
+tmp_tasks = [
+	(func, data['url_data'][key], data['city'], data['language'])
+	for data in url_list
+	for func, key in parsers
+]
+tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
+
+# for data in url_list:
+# 	for func, key in parsers:
+# 		url = data['url_data'][key]
+# 		j, e = func(url, city=data['city'], language=data['language'])
+# 		jobs += j
+# 		errors += e
+
+loop.run_until_complete(tasks)
+loop.close()
+
+for job in jobs:
+	v = Vacancy(**job)
+	try:
+		v.save()
+	except DatabaseError:
+		pass
+
+if errors:
+	er = Error(data=errors).save()
+
+# h = codecs.open('work.txt', 'w', 'utf-8')
+# h.write(str(jobs))
+# h.close()
--- scraping/parsers.py
+++ scraping/parsers.py
@@ -3,7 +3,6 @@ import codecs
 from bs4 import BeautifulSoup as bs
 from random import randint
 
-
 __all__ = ('hh_ru', 'habr_career')
 
 # Эмуляция сервера для получения данных с сайта
@@ -18,72 +17,79 @@ headers = [
 ]
 
 
-def hh_ru(url):
+def hh_ru(url, city=None, language=None):
 	jobs = []
 	errors = []
-	resp = requests.get(url, headers=headers[randint(0, 2)])
-	if resp.status_code == 200:
-		soup = bs(resp.content, 'html.parser')
-		not_found_jobs = soup.find('h1', attrs={'class': 'no-content'})
-		if not not_found_jobs:
-			main_div = soup.find('div', attrs={'id': 'a11y-main-content'})
-			if main_div:
-				div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item__layout'})
-				for div_1 in div_list:
-					div_title_1 = div_1.find('div', attrs={'class': 'vacancy-serp-item-body'})
-					div_title_2 = div_title_1.find('div', attrs={'class': 'vacancy-serp-item-body__main-info'})
-					div_with_title = div_title_2.find('div', attrs={'class': ''})
-					title = div_with_title.find('h3')
-					href = title.a['href']
-					div_desc_1 = div_1.find('div', attrs={'class': 'g-user-content'})
-					div_desc_2 = div_desc_1.find('div', attrs={'class': 'bloko-text'})
-					description = div_desc_2.text
-					div_company_1 = div_title_2
-					div_company_2 = div_company_1.find('div', attrs={'class': 'vacancy-serp-item-company'})
-					div_company_3 = div_company_2.find('div', attrs={'class': 'bloko-text'})
-					div_company_4 = div_company_3.find('div', attrs={'class': 'vacancy-serp-item__meta-info-company'})
-					company = div_company_4.text
-					jobs.append({'title': title.text, 'href': href, 'description': description, 'company': company})
+	if url:
+		resp = requests.get(url, headers=headers[randint(0, 2)])
+		if resp.status_code == 200:
+			soup = bs(resp.content, 'html.parser')
+			not_found_jobs = soup.find('h1', attrs={'class': 'no-content'})
+			if not not_found_jobs:
+				main_div = soup.find('div', attrs={'id': 'a11y-main-content'})
+				if main_div:
+					div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item__layout'})
+					for div_1 in div_list:
+						div_title_1 = div_1.find('div', attrs={'class': 'vacancy-serp-item-body'})
+						div_title_2 = div_title_1.find('div', attrs={'class': 'vacancy-serp-item-body__main-info'})
+						div_with_title = div_title_2.find('div', attrs={'class': ''})
+						title = div_with_title.find('h3')
+						href = title.a['href']
+						div_desc_1 = div_1.find('div', attrs={'class': 'g-user-content'})
+						div_desc_2 = div_desc_1.find('div', attrs={'class': 'bloko-text'})
+						description = div_desc_2.text
+						div_company_1 = div_title_2
+						div_company_2 = div_company_1.find('div', attrs={'class': 'vacancy-serp-item-company'})
+						div_company_3 = div_company_2.find('div', attrs={'class': 'bloko-text'})
+						div_company_4 = div_company_3.find('div',
+														   attrs={'class': 'vacancy-serp-item__meta-info-company'})
+						company = div_company_4.text
+						jobs.append({'title': title.text, 'url': href, 'description': description, 'company': company,
+									 'city_id': city, 'language_id': language}
+									)
+				else:
+					errors.append({'url': url, 'error': 'div не найден'})
 			else:
-				errors.append({'url': url, 'error': 'div не найден'})
+				errors.append({'url': url, 'error': 'Вакансий по данному запросу нет'})
 		else:
-			errors.append({'url': url, 'error': 'Вакансий по данному запросу нет'})
-	else:
-		errors.append({'url': url, 'error': 'Страница не отвечает'})
+			errors.append({'url': url, 'error': 'Страница не отвечает'})
 
 	return jobs, errors
 
 
-def habr_career(url):
+def habr_career(url, city=None, language=None):
 	domain = 'https://career.habr.com'
 	jobs = []
 	errors = []
-	resp = requests.get(url, headers=headers[randint(0, 2)])
-	if resp.status_code == 200:
-		soup = bs(resp.content, 'html.parser')
-		not_found_jobs = soup.find('div', attrs={'class': 'no-content'})
-		if not not_found_jobs:
-			main_div = soup.find('div', attrs={'class': 'section-group section-group--gap-medium'})
-			if main_div:
-				div_list = main_div.find_all('div', attrs={'class': 'vacancy-card'})
-				for div_1 in div_list:
-					div_title_1 = div_1.find('div', attrs={'class': 'vacancy-card__inner'})
-					div_title_2 = div_title_1.find('div', attrs={'class': 'vacancy-card__info'})
-					div_with_title = div_title_2.find('div', attrs={'class': 'vacancy-card__title'})
-					title = div_with_title
-					href = title.a['href']
-					div_desc_1 = div_title_2.find('div', attrs={'class': 'vacancy-card__meta'})
-					description = div_desc_1.text
-					div_company_1 = div_title_2.find('div', attrs={'class': 'vacancy-card__company'})
-					company = div_company_1.text
-					jobs.append(
-						{'title': title.text, 'href': domain + href, 'description': description, 'company': company})
+	if url:
+		resp = requests.get(url, headers=headers[randint(0, 2)])
+		if resp.status_code == 200:
+			soup = bs(resp.content, 'html.parser')
+			not_found_jobs = soup.find('div', attrs={'class': 'no-content'})
+			if not not_found_jobs:
+				main_div = soup.find('div', attrs={'class': 'section-group section-group--gap-medium'})
+				if main_div:
+					div_list = main_div.find_all('div', attrs={'class': 'vacancy-card'})
+					for div_1 in div_list:
+						div_title_1 = div_1.find('div', attrs={'class': 'vacancy-card__inner'})
+						div_title_2 = div_title_1.find('div', attrs={'class': 'vacancy-card__info'})
+						div_with_title = div_title_2.find('div', attrs={'class': 'vacancy-card__title'})
+						title = div_with_title
+						href = title.a['href']
+						div_desc_1 = div_title_2.find('div', attrs={'class': 'vacancy-card__meta'})
+						description = div_desc_1.text
+						div_company_1 = div_title_2.find('div', attrs={'class': 'vacancy-card__company'})
+						company = div_company_1.text
+						jobs.append(
+							{'title': title.text, 'url': domain + href, 'description': description, 'company': company,
+							 'city_id': city, 'language_id': language}
+						)
+				else:
+					errors.append({'url': url, 'error': 'div не найден'})
 			else:
-				errors.append({'url': url, 'error': 'div не найден'})
+				errors.append({'url': url, 'error': 'Вакансий по данному запросу нет'})
 		else:
-			errors.append({'url': url, 'error': 'Вакансий по данному запросу нет'})
-	else:
-		errors.append({'url': url, 'error': 'Страница не отвечает'})
+			errors.append({'url': url, 'error': 'Страница не отвечает'})
 
 	return jobs, errors
 
