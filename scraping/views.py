4530e0e5b4aa7cbb095ef48e18df47497c9541ba
Vadim <vadya.dmitrichev26@gmail.com>
2023-01-31 14:54:32 +0300

    Пагинация + рефактор отображения

--- scraping/views.py
+++ scraping/views.py
@@ -1,15 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .forms import FindForm
 from .models import Vacancy
 
 
 def home(request):
-	'''Функция для отображения вакансий'''
+	"""Функция для отображения вакансий"""
 
+	form = FindForm()
+
+	return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+	"""Функция для отображения вакансий"""
+	page_obj = None
 	form = FindForm()
 	city = request.GET.get('city')
 	language = request.GET.get('language')
 	query = []
+	context = {
+		'city': city,
+		'language': language,
+		'form': form
+	}
 	if city or language:
 		d_filter = {}
 		if city:
@@ -17,4 +31,8 @@ def home(request):
 		if language:
 			d_filter['language__slug'] = language
 		query = Vacancy.objects.filter(**d_filter)
-	return render(request, 'scraping/home.html', {'vacancy_list': query, 'form': form})
+		paginator = Paginator(query, 10)
+		page_number = request.GET.get('page')
+		page_obj = paginator.get_page(page_number)
+		context['vacancy_list'] = page_obj
+	return render(request, 'scraping/list_view.html', context)
--- scraping_service/urls.py
+++ scraping_service/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from scraping.views import home
+from scraping import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', views.home, name='home'),
+    path('list', views.list_view, name='list'),
 ]
